087b7f190faa2e8bae2d46d995d4bc1ae78e0f04
Gem_Ge_Jun     ; ) <jwqe764241@gmail.com>
2017-05-29 19:40:49 +0900

    추가수정
    규칙 추가 및 WinMain 추가 -> 경우를 대비해서 (그럴일은 없겠지만;

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,19 +1,32 @@
 #include <Windows.h>
 
+
+
+
+
 #if defined(_MBCS)
 
 	//For Multubyte
 
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR IpCmdLine, int nCmdShow) {
+
+	///MessageBox(NULL, "YES!!", "TEST", MB_OK);
+
+}
+
 #elif defined(UNICODE) || defined(_UNICODE)
 
-	//For Unicode
+	//For Unicode	->	Use this
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
+
+
+}
 
 #else
 
 	///No Settings 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lCmdLine, int nCmdShow) {
-
-	
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 
 }
 
