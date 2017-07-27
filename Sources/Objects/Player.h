db435afc68022b41dbc5fe2403d6c6df2916c7ab
PRKM <dudgns2486@gmail.com>
2017-07-27 19:48:39 +0900

    Updated ICharacter & Player class

--- Sources/Objects/Player.h
+++ Sources/Objects/Player.h
@@ -1,7 +1,9 @@
 #pragma once
-#include <D3DX11.h>
+
+#include <D3DX10math.h>
 #include "Sources\Objects\IRenderable.h"
 #include "Sources\Objects\ICharacter.h"
+#include "..\Geometries\GameBitmap.h"
 
 class Player : public IRenderable, public ICharacter
 {
@@ -17,13 +19,19 @@ public:
 	virtual void Render() override;
 
 	virtual void Idle() override;
-	virtual void Move(XMVECTORF32 target) override;
-	virtual void Look(XMVECTORF32 direction) override;
+	virtual void Move(D3DXVECTOR3 target) override;
 	virtual void Attack(void** target) override; // attack close target or target attack.
 	//virtual void Attack(XMVECTORF32 direction, Wepon wepon); // long target or non-target attack.
 	virtual void Damage(int damage) override;
 	virtual void Die() override;
 
+	bool IsDied();
 private:
+	D3DXVECTOR3 m_Position;
 
+	int m_Health;
+
+	bool m_IsDied;
+
+	GameBitmap* m_Object;
 };
