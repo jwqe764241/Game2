54a9fc8eb7c26b07f2c9326a0800f24719e640e3
Gem_Ge_Jun     ; ) <jwqe764241@gmail.com>
2017-07-31 00:06:42 +0900

    Player 멤버함수 정의 후 일괄빌드
    여기서 1.0으로 병합함

--- Sources/Assets/Player.cpp
+++ Sources/Assets/Player.cpp
@@ -9,3 +9,70 @@ Player::~Player()
 {
 
 }
+
+void Player::Load(ID3D11Device * device, int screenWidth, int screenHeight, int bitmapWidth, int bitmapHeight)
+{
+
+}
+
+void Player::Release()
+{
+
+}
+
+void Player::Reset()
+{
+
+}
+
+void Player::Update(float dt)
+{
+
+}
+
+void Player::Update(float dt, int keyCode)
+{
+
+}
+
+void Player::Render(ID3D11DeviceContext * deviceContext)
+{
+
+}
+
+void Player::Idle()
+{
+
+}
+
+void Player::Move(XMVECTORF32 target)
+{
+
+}
+
+void Player::Look(XMVECTORF32 direction)
+{
+
+}
+
+void Player::Attack(void ** target)
+{
+
+}
+
+void Player::Damage(int damage)
+{
+
+}
+
+void Player::Die()
+{
+
+}
+
+bool Player::isDied()
+{
+
+	return false;
+}
+
